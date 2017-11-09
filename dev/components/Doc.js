4bdd849b50ec910f347fe618925c69e017371428
Luis Rojas <lerl221295@gmail.com>
2017-11-09 17:05:35 -0400

    feat(jwt en el header):

---
+++ dev/components/Doc.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+import { API_URL } from "../apolloClient";
+import { getUser } from "../Authenticate";
+import 'graphiql/graphiql.css';
+
+const graphQLFetcher = (graphQLParams) => {
+    const user = getUser();
+    return(
+        fetch(`http://${API_URL}/graphql`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept-Type': 'application/json',
+                'Authorization':  user ? `Bearer ${user.token}` : null
+            },
+            body: JSON.stringify(graphQLParams),
+        }).then(response => response.json())
+    )
+}
+
+export default () => (
+    <div style={{height: "100vh"}}>
+        <GraphiQL fetcher={graphQLFetcher} />
+    </div>
+)
