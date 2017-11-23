a142751989b34afac5b55679343d51759b9d7e41
Luis Rojas <lerl221295@gmail.com>
2017-11-23 16:48:04 -0400

    feat(tickets): add tickets list and container

--- dev/modules/ticket/components/containers/TicketsList.js
+++ dev/modules/ticket/components/containers/TicketsList.js
@@ -1,8 +1,61 @@
 import { connect } from 'react-redux'
+import { graphql } from "react-apollo"
+import GetTickets from '../../graphql/querys/tickets.graphql'
 import TicketList from '../presentationals/TicketsList'
 
-const mapStateToProps = (state) => ({
-	filter_form : state.form.FilterForm.values
-});
+const limit = 10;
+const TicketWithApolloData = graphql(GetTickets, {
+	options: ({filter_form}) => ({
+		variables: {
+			filter: filter_form,
+			limit
+		},
+		notifyOnNetworkStatusChange: true
+	}),
+	props: ({ ownProps, data: { fetchMore, refetch, tickets, loading, error } }) => {
+		//console.log("tickets",tickets)
+		let ticketsArray;
+		if(tickets) ticketsArray = tickets.nodes;
+		return({
+			...ownProps,
+			tickets: ticketsArray,
+			loading,
+			error,
+			refetchTickets: () => refetch({ limit, filter: ownProps.filter_form }),
+			loadMoreTickets: () => fetchMore({
+				variables: {
+					limit,
+					offset: ticketsArray.length
+				},
+				updateQuery: (previousResult, { fetchMoreResult }) => {
+					if (!fetchMoreResult) return previousResult;
+					return Object.assign({}, previousResult, {
+						tickets: {
+							__typename: "TicketsResponse",
+							nodes: [...previousResult.tickets.nodes, ...fetchMoreResult.tickets.nodes]
+						}
+					});
+				}
+			})
+		})
+	}
+})(TicketList)
 
-export default connect(mapStateToProps)(TicketList)
+/*Considerar crear otro componente intermediario, el cual sera wrapeado por TicketWithFilters para obtener
+* todos los campos del formulario de filtrado, pero pasarlos a TicketWithApolloData como props solo cuando se
+* desea hacer refetch de los tickets en base al nuevo estado del form (y no siempre como funciona ahorita)*/
+
+const mapStateToProps = ({form: {FilterForm: {values}}}) => {
+	if(values){
+		const keysForMap = ["agents", "clients", "organizations", "suppliers", "groups"];
+		let filterMapedObject = {};
+		for(let key of keysForMap){
+			if(values[key]) filterMapedObject[key] = values[key].map(ob => ob.id);
+		}
+		return({filter_form: {...values, ...filterMapedObject} });
+	}
+	return({filter_form: values})
+};
+const TicketWithFilters = connect(mapStateToProps)(TicketWithApolloData);
+
+export default TicketWithFilters
--- dev/modules/ticket/components/presentationals/TicketsList.js
+++ dev/modules/ticket/components/presentationals/TicketsList.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react'
+import {List, ListItem, Paper, Subheader, Divider, Avatar} from "material-ui";
+import {typography} from 'material-ui/styles';
+import {grey400, cyan600, white} from 'material-ui/styles/colors';
+import Face from 'material-ui/svg-icons/action/face'
+
+const styles = {
+	subheader: {
+		fontSize: 24,
+		fontWeight: typography.fontWeightLight,
+		backgroundColor: cyan600,
+		color: white
+	}
+};
 
 class TicketList extends Component {
-	static defaultProps = {
+	/*static defaultProps = {
 		filter_form: {
 			firstName: "",
 			lastName: ""
 		}
-	}
+	}*/
 
 	render = () => {
-		console.log(this.props.filter_form);
-		const { firstName, lastName } = this.props.filter_form;
-		return(<h1>hola {`${firstName || ''} ${lastName || ''}`}</h1>)
+		const {tickets, loading} = this.props;
+		if(loading && !tickets) return <h1>Cargando...</h1>
+		return(
+			<Paper>
+				<List>
+					<Subheader style={styles.subheader}>Tickets</Subheader>
+					{
+						tickets.map(ticket =>
+							<div key={ticket.number}>
+								<ListItem
+									leftAvatar={
+										do {
+											if(!ticket.client.face_base64) <Avatar icon={<Face/>} />
+											else <Avatar src={ticket.client.face_base64} />
+										}
+									}
+									primaryText={`#${ticket.number}: ${ticket.title}`}
+									secondaryText={ticket.client.fullName}
+									//rightIconButton={rightIconMenu}
+								/>
+								<Divider inset={true} />
+							</div>
+						)
+					}
+				</List>
+			</Paper>
+		)
 	}
 }
 
