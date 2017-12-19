9f49ac5dbc9df030d3ecd63805d859501df93822
Luis Eduardo Rojas <lerl221295@gmail.com>
2017-12-19 17:25:33 -0400

    Show title on header and change state label

--- dev/common/components/Header.js
+++ dev/common/components/Header.js
@@ -19,6 +19,7 @@ const GetHeaderTitle = (pathname) => {
 		do {
 			if (pathname === '/') {'Dashboard'} else
 			if (pathname === '/tickets') {'Tickets'} else
+			if (pathname.includes("tickets/")) {'Detalle del Ticket'} else
 			if (pathname === '/clients') {'Clientes y Organizaciones'} else
 			if (pathname === '/clients/new') {'Creación de nuevo cliente'} else
 			if (pathname === '/clients/organizations/new') {'Creación de nueva organización'} else
@@ -31,6 +32,7 @@ const GetHeaderTitle = (pathname) => {
 			if (pathname === '/admin/agents/groups/new') {'Creación de nuevo grupo'} else
 			if (pathname.includes('/admin/agents/groups/')) {'Detalle y edición de grupo'} else
 			if (pathname.includes('/admin/agents/')) {'Detalle y edición de agente'} else
+			if (pathname === '/admin/ticketFields') {'Configuraciones del Ticket'} else
 			if (pathname === '/admin/sla') {'Contratos a Nivel de Servicio'} else
 			if (pathname === '/admin/sla/new') {'Creación de nueva política SLA'} else
 			if (pathname.includes('/admin/sla/')) {'Detalle y edición de política SLA'} else
